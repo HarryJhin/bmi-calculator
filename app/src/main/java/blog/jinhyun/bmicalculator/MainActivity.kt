d5a4b00efaeb8a07f75d48ad85040a1bf84b3e34
jjh <joojinhyun00@gmail.com>
2022-05-11 19:12:11 +0900

    뷰바인딩 객체 얻기

--- app/src/main/java/blog/jinhyun/bmicalculator/MainActivity.kt
+++ app/src/main/java/blog/jinhyun/bmicalculator/MainActivity.kt
@@ -2,10 +2,21 @@ package blog.jinhyun.bmicalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import blog.jinhyun.bmicalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    /**
+     * 뷰바인딩 객체 얻기
+     */
+    private val binding by lazy {
+        // `ActivityMainBinding` 클래스는 `activity_main.xml`의 이름을 참고하여 뷰바인딩 설정에 의해 자동 생성
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // `R.layout.activity_main` 같은 효과
+        setContentView(binding.root)
     }
 }
