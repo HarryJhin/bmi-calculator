bfda6329efd08d02f70399bd98de086391709628
jjh <joojinhyun00@gmail.com>
2022-05-11 22:07:41 +0900

    마지막에 입력한 height, weight 값 저장하는 메서드 추가

--- app/src/main/java/blog/jinhyun/bmicalculator/MainActivity.kt
+++ app/src/main/java/blog/jinhyun/bmicalculator/MainActivity.kt
@@ -3,6 +3,7 @@ package blog.jinhyun.bmicalculator
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.preference.PreferenceManager
 import blog.jinhyun.bmicalculator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +25,12 @@ class MainActivity : AppCompatActivity() {
         binding.resultButton.setOnClickListener {
             // 결과 버튼이 클릭되면 할 일
             if (binding.weightEditText.text.isNotBlank() && binding.heightEditText.text.isNotBlank()) {
+                // 마지막에 입력한 키, 몸무게 값 저장
+                saveData(
+                    binding.heightEditText.text.toString().toFloat(),
+                    binding.weightEditText.text.toString().toFloat(),
+                )
+
                 val intent = Intent(this, ResultActivity::class.java).apply {
                     putExtra("weight", binding.weightEditText.text.toString().toFloat())
                     putExtra("height", binding.heightEditText.text.toString().toFloat())
@@ -32,4 +39,15 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    // 데이터를 저장하는 함수
+    private fun saveData(height: Float, weight: Float) {
+        // 프리퍼런스 객체 얻기
+        val preference = PreferenceManager.getDefaultSharedPreferences(this)
+        // 프리퍼런스 객체의 에디터 얻기
+        val editor = preference.edit()
+
+        editor.putFloat("KEY_HEIGHT", height).putFloat("KEY_WEIGHT", weight).apply()
+    }
 }
+
